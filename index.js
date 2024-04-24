0534b66f97bd766b1cb462587ed729ce5ac54897
developmentd420 <development.d420@gmail.com>
2024-04-24 13:06:01 +0600

    Redesign project

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
-
+const port = 8000;
 
 let customers = [];
-let products = []
+let products = [];
 
 // Middleware to set public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Middleware to parse form submissions
 app.use(express.json());
@@ -20,47 +19,55 @@ const saveData = (data, file) => {
       console.error(error);
       return;
     }
-    const jsonData = JSON.stringify(data, null, 2)
-    FileSystem.writeFile(file, jsonData, finished)
-  }
-}
+    const jsonData = JSON.stringify(data, null, 2);
+    FileSystem.writeFile(file, jsonData, finished);
+  };
+};
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.post('/', (req, res) => {
-  res.render('index.ejs');
-})
-
-app.get('/customers', async (req, res) => {
-  res.render('customers.ejs')
+app.post("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/order', (req, res) => {
-  res.render('order.ejs');
+app.get("/login", async (req, res) => {
+  res.render("login.ejs");
 });
 
-app.get('/add_customer', (req, res) => {
-  res.render('add_customer.ejs');
+app.get("/signup", async (req, res) => {
+  res.render("signup.ejs");
 });
 
-app.post('/customers', (req, res) => {
-  customerName = req.body['name'];
-  freightRate = req.body['freightRate'];
-  markUp = req.body['markUp'];
-  res.render('customers.ejs', { data: {
-    name: customerName,
-    freight: freightRate,
-    mark: markUp
-  }});
+app.get("/customers", async (req, res) => {
+  res.render("customers.ejs");
 });
 
-app.get('/add_product', (req, res) => {
-
-  res.render('add_product.ejs');
+app.get("/order", (req, res) => {
+  res.render("order.ejs");
 });
 
+app.get("/add-customer", (req, res) => {
+  res.render("add_customer.ejs");
+});
+
+app.post("/customers", (req, res) => {
+  customerName = req.body["name"];
+  freightRate = req.body["freightRate"];
+  markUp = req.body["markUp"];
+  res.render("customers.ejs", {
+    data: {
+      name: customerName,
+      freight: freightRate,
+      mark: markUp,
+    },
+  });
+});
+
+app.get("/add-product", (req, res) => {
+  res.render("add_product.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
