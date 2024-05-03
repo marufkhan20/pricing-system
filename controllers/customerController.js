8a51d93ba8f89cd5d5d0c7c5968fa9bef656d285
developmentd420 <development.d420@gmail.com>
2024-05-04 00:12:08 +0600

    Implement customer edit functionality

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -139,6 +139,130 @@ export const addNewCustomerController = async (req, res) => {
   }
 };
 
+// edit customer view controller
+export const editCustomerViewController = async (req, res) => {
+  try {
+    const { id } = req.params || {};
+
+    // get the customers data
+    fs.readFile("data/customers.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error reading file");
+        return;
+      }
+
+      // Parse JSON data
+      const customers = JSON.parse(data);
+
+      const customer = customers?.find((customer) => customer?.id === id);
+
+      req.flash("id", customer?.id);
+      req.flash("name", customer?.name);
+      req.flash("freightRate", customer?.freightRate);
+      req.flash("markUp", customer?.markUp);
+      req.flash("commission1", customer?.commission1);
+      req.flash("commission2", customer?.commission2);
+
+      res.render("edit_customer.ejs", {
+        path: "customers",
+        title: "Edit Customer",
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Server error occurred!!",
+    });
+  }
+};
+
+// edit customer controller
+export const editCustomerController = async (req, res) => {
+  try {
+    const { name, freightRate, markUp, commission1, commission2 } =
+      req.body || {};
+
+    const { id } = req.params || {};
+
+    // check validation error
+    const validationErrors = {};
+
+    if (!name) {
+      validationErrors.name = "Name is required!!";
+    }
+
+    if (!freightRate) {
+      validationErrors.freightRate = "Freight rate is required!!";
+    }
+
+    if (!markUp) {
+      validationErrors.markUp = "Mark up is required!!";
+    }
+
+    if (!commission1) {
+      validationErrors.commission1 = "Commission 1 is required!!";
+    }
+
+    if (!commission2) {
+      validationErrors.commission2 = "Commission 2 is required!!";
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      req.flash("name", name);
+      req.flash("freightRate", freightRate);
+      req.flash("markUp", markUp);
+      req.flash("commission1", commission1);
+      req.flash("commission2", commission2);
+      req.flash("errors", JSON.stringify(validationErrors));
+      return res.redirect("/add-customer");
+    }
+
+    // get the customers data
+    fs.readFile("data/customers.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error reading file");
+        return;
+      }
+
+      // Parse JSON data
+      const customers = JSON.parse(data);
+
+      const updatedCustomers = customers?.map((customer) => {
+        if (customer?.id === id) {
+          return {
+            ...customer,
+            ...req.body,
+          };
+        } else {
+          return customer;
+        }
+      });
+
+      // add new customer data
+      fs.writeFile(
+        "data/customers.json",
+        JSON.stringify(updatedCustomers),
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send("Error writing to file");
+            return;
+          }
+
+          res.redirect("/customers");
+        }
+      );
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Server error occurred!!",
+    });
+  }
+};
+
 // delete customer controller
 export const deleteCustomerController = async (req, res) => {
   try {
--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
   addNewCustomerController,
   deleteCustomerController,
+  editCustomerController,
+  editCustomerViewController,
   getAllCustomersController,
   getCustomerController,
 } from "../controllers/customerController.js";
@@ -9,19 +11,26 @@ import privateRoute from "../middlewares/privateRoute.js";
 
 const router = Router();
 
+// get all customers
 router.get("/customers", privateRoute, getAllCustomersController);
 
+// add new customer
 router.get("/add-customer", privateRoute, (req, res) => {
   res.render("add_customer.ejs", {
     path: "customers",
     title: "Add Customer",
   });
 });
-
-router.get("/customers/:id", privateRoute, getCustomerController);
-
 router.post("/add-customer", privateRoute, addNewCustomerController);
 
+// get single customer
+router.get("/customers/:id", privateRoute, getCustomerController);
+
+// edit customer
+router.get("/edit-customer/:id", privateRoute, editCustomerViewController);
+router.post("/edit-customer/:id", privateRoute, editCustomerController);
+
+// delete customer
 router.delete("/customers/:id", privateRoute, deleteCustomerController);
 
 export default router;
